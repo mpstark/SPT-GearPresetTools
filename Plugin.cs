d719f92c8a041b649ceddd143e93c1f98b1f79dc
Michael P. Starkweather <git@mpstark.com>
2024-05-03 18:08:25 -0500

    fix regression with making generic

--- Plugin.cs
+++ Plugin.cs
@@ -7,6 +7,7 @@ using Aki.Reflection.Utils;
 using AutoSaveGearPreset.Patches;
 using HarmonyLib;
 using EFT.Builds;
+using System.Linq;
 
 namespace AutoSaveGearPreset
 {
@@ -17,15 +18,17 @@ namespace AutoSaveGearPreset
         public static Plugin Instance;
         public static ManualLogSource Log => Instance.Logger;
 
-        public static FieldInfo SessionEquipmentBuildStorageField = AccessTools.Field(typeof(ISession), "EquipmentBuildsStorage");
-        public static Type BuildStorageType = SessionEquipmentBuildStorageField.GetType();
-        public static MethodInfo BuildStorageFindCustomBuildByNameMethod = AccessTools.Method(BuildStorageType, "FindCustomBuildByName");
-        public static MethodInfo BuildStorageSaveBuildMethod = AccessTools.Method(BuildStorageType, "SaveBuild");
-        public static Type BuildType = BuildStorageSaveBuildMethod.GetParameters()[0].ParameterType;
-        public static PropertyInfo BuildIdProperty = AccessTools.Property(BuildType, "Id");
-        public static PropertyInfo SessionProfileProperty = AccessTools.Property(typeof(ISession), "Profile");
-        public static ISession Session => ClientAppUtils.GetMainApp().GetClientBackEndSession();
-        public static Profile PlayerProfile => SessionProfileProperty.GetValue(Session) as Profile;
+        private static PropertyInfo _sessionEquipmentBuildStorageProperty;
+        private static Type _buildStorageType;
+        private static MethodInfo _buildStorageFindCustomBuildByNameMethod;
+        private static MethodInfo _buildStorageSaveBuildMethod;
+        private static Type _buildType;
+        private static PropertyInfo _buildIdProperty;
+        private static Type _profileInterface;
+        private static PropertyInfo _sessionProfileProperty;
+
+        private static ISession _session => ClientAppUtils.GetMainApp().GetClientBackEndSession();
+        private static Profile _playerProfile => _sessionProfileProperty.GetValue(_session) as Profile;
 
         public static string AutoSaveKitName = "Last Kit";
 
@@ -34,28 +37,38 @@ namespace AutoSaveGearPreset
             Instance = this;
             DontDestroyOnLoad(this);
 
+            // reflection to make generic
+            _sessionEquipmentBuildStorageProperty = AccessTools.Property(typeof(ISession), "EquipmentBuildsStorage");
+            _buildStorageType = _sessionEquipmentBuildStorageProperty.PropertyType;
+            _buildStorageFindCustomBuildByNameMethod = AccessTools.Method(_buildStorageType, "FindCustomBuildByName");
+            _buildStorageSaveBuildMethod = AccessTools.Method(_buildStorageType, "SaveBuild");
+            _buildType = _buildStorageSaveBuildMethod.GetParameters()[0].ParameterType;
+            _buildIdProperty = AccessTools.Property(_buildType, "Id");
+            _profileInterface = typeof(ISession).GetInterfaces().First(x => x.GetProperties().Length == 2 && AccessTools.Property(x, "Profile") != null);
+            _sessionProfileProperty = AccessTools.Property(_profileInterface, "Profile");
+
             // patches
             new MatchMakerTimeHasComeShowPatch().Enable();
         }
 
         public static void TrySaveCurrentGear()
         {
-            SaveCurrentEquipmentAsBuild(AutoSaveKitName);
+            SaveEquipmentAsBuild(AutoSaveKitName, _playerProfile.Inventory.Equipment);
         }
 
         private static object FindCustomBuildByName(string name)
         {
-            var buildStorage = SessionEquipmentBuildStorageField.GetValue(Session);
-            return BuildStorageFindCustomBuildByNameMethod.Invoke(buildStorage, new object[] { name });
+            var buildStorage = _sessionEquipmentBuildStorageProperty.GetValue(_session);
+            return _buildStorageFindCustomBuildByNameMethod.Invoke(buildStorage, new object[] { name });
         }
 
         private static void SaveBuild(object build)
         {
-            var buildStorage = SessionEquipmentBuildStorageField.GetValue(Session);
-            BuildStorageSaveBuildMethod.Invoke(buildStorage, new[] { build });
+            var buildStorage = _sessionEquipmentBuildStorageProperty.GetValue(_session);
+            _buildStorageSaveBuildMethod.Invoke(buildStorage, new[] { build });
         }
 
-        private static void SaveCurrentEquipmentAsBuild(string name)
+        private static void SaveEquipmentAsBuild(string name, EquipmentClass equipment)
         {
             // INSTEAD OF THIS
             // var oldKit = Session.EquipmentBuildsStorage.FindCustomBuildByName(AutoSaveKitName);
@@ -65,12 +78,12 @@ namespace AutoSaveGearPreset
 
             var oldKit = FindCustomBuildByName(name);
             var mongoID = (oldKit == null)
-                            ? new MongoID(PlayerProfile)
-                            : (MongoID)BuildIdProperty.GetValue(oldKit);
-            var newKit = Activator.CreateInstance(BuildType, new object[] { mongoID,
-                                                                            AutoSaveKitName,
-                                                                            PlayerProfile.Inventory.Equipment,
-                                                                            EEquipmentBuildType.Custom }); 
+                            ? new MongoID(_playerProfile)
+                            : (MongoID)_buildIdProperty.GetValue(oldKit);
+            var newKit = Activator.CreateInstance(_buildType, new object[] { mongoID,
+                                                                             AutoSaveKitName,
+                                                                             equipment,
+                                                                             EEquipmentBuildType.Custom }); 
             SaveBuild(newKit);
         }
     }
