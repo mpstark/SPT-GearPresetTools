ea95c6b3acc4ef96d21c85bd53192063056e8353
Michael P. Starkweather <git@mpstark.com>
2024-05-03 04:52:01 -0500

    working prototype

---
+++ Patches/MatchmakerTimeHasComeShowPatch.cs
@@ -0,0 +1,22 @@
+﻿using Aki.Reflection.Patching;
+using EFT;
+using EFT.UI.Matchmaker;
+using HarmonyLib;
+using System.Reflection;
+
+namespace AutoSaveGearPreset.Patches
+{
+    internal class MatchMakerTimeHasComeShowPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(MatchmakerTimeHasCome), nameof(MatchmakerTimeHasCome.Show), new[] {typeof(ISession), typeof(RaidSettings)});
+        }
+
+        [PatchPostfix]
+        public static void PatchPostfix()
+        {
+            Plugin.Instance.TrySaveCurrentGear();
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,13 @@
-﻿using DrakiaXYZ.VersionChecker;
+﻿using System;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using EFT;
+using Aki.Reflection.Utils;
+using DrakiaXYZ.VersionChecker;
 using AutoSaveGearPreset.Patches;
+using HarmonyLib;
+using EFT.Builds;
 
 namespace AutoSaveGearPreset
 {
@@ -10,8 +18,11 @@ namespace AutoSaveGearPreset
         public const int TarkovVersion = 29197;
         public static Plugin Instance;
         public static ManualLogSource Log => Instance.Logger;
-        public static Inventory PlayerInventory => ClientAppUtils.GetMainApp().GetClientBackEndSession().Profile.Inventory;
-        public static string PluginFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public static ConstructorInfo BuildConstructor;
+        public static MethodInfo SaveBuildMethod;
+
+        public static string AutoSaveKitName = "Last Kit";
 
         internal void Awake()
         {
@@ -24,6 +35,30 @@ namespace AutoSaveGearPreset
             DontDestroyOnLoad(this);
 
             // patches
+            new MatchMakerTimeHasComeShowPatch().Enable();
+        }
+
+        internal void TrySaveCurrentGear()
+        {
+            var session = ClientAppUtils.GetMainApp().GetClientBackEndSession();
+
+            // find methodinfo and constructorinfo for out methods to avoid using GClass3182 directly
+            if (BuildConstructor == null)
+            {
+                SaveBuildMethod = AccessTools.Method(session.EquipmentBuildsStorage.GetType(), "SaveBuild");
+                BuildConstructor = AccessTools.Constructor(SaveBuildMethod.GetParameters()[0].ParameterType, new[] { typeof(MongoID), typeof(string), typeof(EquipmentClass), typeof(EEquipmentBuildType) });
+            }
+
+            // try to get the last preset with that name
+            var oldKit = session.EquipmentBuildsStorage.FindCustomBuildByName(AutoSaveKitName);
+            var mongoID = oldKit == null ? new MongoID(session.Profile) : oldKit.Id;
+
+            // var newKit = new GClass3182(mongoID, AutoSaveKitName, session.Profile.Inventory.Equipment, EEquipmentBuildType.Custom);
+            // session.EquipmentBuildsStorage.SaveBuild(newKit);
+
+            // cumbersome to do without using the class directly
+            var newKit = BuildConstructor.Invoke(new object[] { mongoID, AutoSaveKitName, session.Profile.Inventory.Equipment, EEquipmentBuildType.Custom });
+            SaveBuildMethod.Invoke(session.EquipmentBuildsStorage, new[] { newKit });
         }
     }
 }
